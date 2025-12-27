f948e424984416ffa9c9df377054382c5eb25604
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-12-27 18:19:55 +0530

    we create serializers for coments app

---
+++ server/apps/tickets/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from apps.tickets.models import Issue
+
+
+class IssueSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Issue
+        fields = "__all__"
+        read_only_fields = ("id", "created_at", "updated_at")
