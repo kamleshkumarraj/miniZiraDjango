dadb15c6ce6c85322b2a54dc867e9dda9e558dda
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-12-27 17:10:02 +0530

    we create models for organizations

--- server/apps/organizations/models.py
+++ server/apps/organizations/models.py
@@ -1,3 +1,65 @@
+import uuid
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Organization(models.Model):
+    """
+    Jira-style Organization (Tenant)
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(max_length=150)
+    key = models.CharField(max_length=20, unique=True)   # e.g. MINIZIRA
+
+    domain = models.CharField(
+        max_length=100,
+        unique=True,
+        help_text="Company email domain (minizira.io)"
+    )
+
+    description = models.TextField(blank=True)
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="owned_organizations",
+    )
+
+    plan = models.CharField(
+        max_length=30,
+        choices=[
+            ("FREE", "Free"),
+            ("PRO", "Pro"),
+            ("ENTERPRISE", "Enterprise"),
+        ],
+        default="FREE",
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("ACTIVE", "Active"),
+            ("SUSPENDED", "Suspended"),
+            ("ARCHIVED", "Archived"),
+        ],
+        default="ACTIVE",
+    )
+
+    timezone = models.CharField(max_length=50, default="UTC")
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+    max_users = models.PositiveIntegerField(default=10)
+    max_projects = models.PositiveIntegerField(default=5)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "organizations"
+
+    def __str__(self):
+        return f"{self.name} ({self.key})"
